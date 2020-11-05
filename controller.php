6b17d8e7b5e47c2e460036e0eb89545a2a002f6a
Marco Kuoni <marco@lemonbrain.ch>
2020-11-05 18:45:57 +0100

    dependency to websocket

--- controller.php
+++ controller.php
@@ -17,10 +17,10 @@ class Controller extends Package
      * @see \Concrete\Core\Package\Package::$packageDependencies
      */
     protected $packageDependencies = [
-        'concrete5_graphql_websocket' => '1.3.2'
+        'concrete5_graphql_websocket' => '2.0.0'
     ];
     protected $appVersionRequired = '8.5.1';
-    protected $pkgVersion = '4.0.5';
+    protected $pkgVersion = '4.0.6';
     protected $pkgHandle = 'concrete5_graphql_websocket_security';
     protected $pkgName = 'GraphQL with Websocket Security';
     protected $pkgDescription = 'Helps to use GraphQL and Websocket in Concrete5 securley';
